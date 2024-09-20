e6baca267cd35199f02f03b91a732de30d019161
artiomyukhimenko <artiomyukhimenko888@gmail.com>
2024-09-20 11:49:06 +0300

    Update lab1.cpp

--- lab1.cpp
+++ lab1.cpp
@@ -1,2 +1,12 @@
-#Yukhimenko_Lab1
-https://online.fliphtml5.com/zdyiy/sozq/
+Код- #include <math.h>
+#include <iostream>   
+using namespace std;
+int main() {
+	int a;
+	int b;
+	cin >> a;
+	cin >> b;
+	if (a % b == 0) { cout << "yes"; }
+	else { cout << "no"; }
+}
+
